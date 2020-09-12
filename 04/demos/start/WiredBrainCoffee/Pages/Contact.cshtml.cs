2354f646c88af825f76a6ede0f71907f1805b0ce
Kevin Alama <kalama@xirectss.com>
2020-09-12 08:06:18 -0500

    first commit

---
+++ 04/demos/start/WiredBrainCoffee/Pages/Contact.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffee.Models;
+
+namespace WiredBrainCoffee.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public Contact Contact { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
