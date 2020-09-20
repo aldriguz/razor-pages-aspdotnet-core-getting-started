9ca3be2b37f97d5bf9114fa7f24ac745615aee88
Kevin Alama <alama1992@gmail.com>
2020-09-20 12:06:23 -0500

    dependency injection basis

--- Demo/WiredBrainCoffee/Pages/Menu.cshtml.cs
+++ Demo/WiredBrainCoffee/Pages/Menu.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using WiredBrainCoffee.Models;
 using WiredBrainCoffee.Services;
 
@@ -10,13 +11,30 @@ namespace WiredBrainCoffee.Pages
 {
     public class MenuModel : PageModel
     {
+        private IMenuService menuService;
+        private ILogger<MenuModel> logger;
+
         public List<MenuItem> Menu { get; set; }
+
+        public MenuModel(IMenuService menuService, ILogger<MenuModel> logger)
+        {
+            this.menuService = menuService;
+            this.logger = logger;
+        }
 
         public void OnGet()
         {
-            var menuService = new MenuService();
+            try
+            {
+                Menu = menuService.GetMenuItems();
+                throw new Exception();
+            }
+            catch(Exception ex)
+            {
+                logger.Log(LogLevel.Debug, "Could not retrieve menu");
+            }
 
-            Menu = menuService.GetMenuItems();
+            
         }
     }
 }
--- Demo/WiredBrainCoffee/Startup.cs
+++ Demo/WiredBrainCoffee/Startup.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WiredBrainCoffee.Services;
 
 namespace WiredBrainCoffee
 {
@@ -32,11 +36,31 @@ namespace WiredBrainCoffee
             {
                 options.ConstraintMap.Add("promo", typeof(PromoConstraint));
             });
+
+            services.AddScoped<IMenuService, MenuService>();
+            services.AddLogging();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.Contains("alive"))
+                {
+                    UnicodeEncoding uniencoding = new UnicodeEncoding();
+                    byte[] outputMsg = uniencoding.GetBytes("The app is alive");
+                    //research how to print on screen instead of download file
+                    await context.Response.Body.WriteAsync(outputMsg, 0, outputMsg.Length);
+                }
+                else
+                {
+                    Debug.WriteLine("Before Razor pages");
+                    await next.Invoke();
+                    Debug.WriteLine("After Razor pages");
+                }
+            });
+            
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
